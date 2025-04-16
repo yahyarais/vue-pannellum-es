1b9f4d81d6c7da0d9fa83714f83b675b70dfd15a
Yahya Rais <yahya-rais@live.com>
2025-04-16 08:43:53 +0700

    refactor: update Vite config to use command-based configuration

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,30 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/vue-pannellum-es.vue'),
-      name: 'VuePannellum',
-      fileName: (format) => `vue-pannellum-es.${format}.js`,
-    },
-    rollupOptions: {
-      external: ['vue', 'pannellum'],
-      output: {
-        globals: {
-          vue: 'Vue',
-          pannellum: 'pannellum'
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [vue()],
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/vue-pannellum-es.vue'),
+        name: 'VuePannellum',
+        fileName: (format) => `vue-pannellum-es.${format}.js`,
+      },
+      rollupOptions: {
+        external: ['vue', 'pannellum'],
+        output: {
+          globals: {
+            vue: 'Vue',
+            pannellum: 'pannellum'
+          }
         }
-      }
-    },
+      },
+    }
   }
+
+  if (command === 'serve') {
+    config.root = 'demo'
+  }
+
+  return config
 })
